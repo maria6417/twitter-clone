a9c527e18b8e047b8c9da27c07c85a2d7dbee209
hmaria <mh6417a@gmail.com>
2022-11-02 23:09:23 -0700

    created widget component

--- src/Widgets.jsx
+++ src/Widgets.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 import './Widgets.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Widgets(props) {
   return (
     <div className="widgets">
-      Widgets
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening?</h2>
+        <TwitterTweetEmbed tweetId={"1579747335743176705"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="elonmusk"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://github.com/maria6417"}
+          options={{ text:"#reactjs is awesome", via: "maria"}}
+        />
+      </div>
     </div>
   )
 }
